998deba6e44b8b08a0b922a33eab2c846f1f6c5c
Promit Barua Antu <promitbarua796@gmail.com>
2025-11-11 01:09:44 +0600

    need to fix error on connect page.svelte

---
+++ src/routes/account/connect/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	// import type { PageProps } from './$types';
+
+	// let { data }: PageProps = $props();
+	import * as Card from '$lib/components/ui/card';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+</script>
+
+<section class="my-50 flex items-center justify-center">
+	<Card.Root class="w-full max-w-lg">
+		<Card.Header>
+			<Card.Title>Log into your account</Card.Title>
+			<Card.Description>Enter your email and password and get logged in!</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<!-- <form>
+				<div class="flex flex-col gap-6">
+					<div class="grid gap-2">
+						<Label for="email">Email</Label>
+						<Input id="email" type="email" placeholder="m@example.com" required />
+					</div>
+					<div class="grid gap-2">
+						<div class="flex items-center">
+							<Label for="password">Password</Label>
+							<a href="##" class="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+								Forgot your password?
+							</a>
+						</div>
+						<Input id="password" type="password" required />
+					</div>
+				</div>
+			</form> -->
+		</Card.Content>
+		<Card.Footer>
+			<p>Card Footer</p>
+		</Card.Footer>
+	</Card.Root>
+</section>
