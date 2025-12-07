51a64424931b318160f7b6e787302556878cf321
Promit Barua Antu <promitbarua796@gmail.com>
2025-12-07 21:25:46 +0600

    just added some in navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,4 +1,20 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+
 	let { user } = $props();
+	// let user = fals`e;
 	console.log('user', user);
 </script>
+
+<nav class="">
+	<div><h1>FacePaw</h1></div>
+	{#if user}
+		<div>
+			{user.name}
+		</div>
+	{:else}
+		<div>
+			<Button>Log In</Button>
+		</div>
+	{/if}
+</nav>
