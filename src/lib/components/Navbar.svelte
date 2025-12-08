d54901d53343a32e3fd5650ff02161b228fb6aa2
Promit Barua Antu <promitbarua796@gmail.com>
2025-12-08 21:27:16 +0600

    need to fix sub nav layout

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,20 +4,29 @@
 	let { user } = $props();
 	// let user = fals`e;
 	console.log('user', user);
-	function toggleMenu() {
-
-	}
+	function toggleMenu() {}
 </script>
 
-<nav class="m-5 flex flex-row justify-between">
-	<div><h1>FacePaw</h1></div>
+<nav class="relative m-5 bg-neutral-300">
+	<div class="absolute left-1"><h1>FacePaw</h1></div>
 	{#if user}
-		<Button class="h-5 w-5 rounded-full bg-amber-200 text-center cursor-pointer" onclick={toggleMenu}>
+		<Button
+			class="absolute right-1 h-5 w-5 cursor-pointer rounded-full bg-amber-200 p-5 text-center transition-colors delay-300"
+			onclick={toggleMenu}
+		>
 			{user.name[0]}
 		</Button>
 		<aside>
 			<nav>
-				
+				<ul>
+					<li>
+						<Button>Log Out</Button>
+					</li>
+					<li><a href="/profile">Profile</a></li>
+					<li><a href="/posts">Posts</a></li>
+					<li><a href="/home">Home</a></li>
+					<li><a href="/create">Create</a></li>
+				</ul>
 			</nav>
 		</aside>
 	{:else}
