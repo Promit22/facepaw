5e25e1492692b41f80b6020e8f6ed43144bc158e
Promit Barua Antu <promitbarua796@gmail.com>
2026-02-07 17:15:58 +0600

    update

--- src/lib/server/models/users.ts
+++ src/lib/server/models/users.ts
@@ -1,5 +1,7 @@
 import type { User } from '$lib/types/user';
 import { db } from '../db/db';
+import bcrypt from 'bcrypt';
+
 export function createUser(imagePath: string, name: string, email: string, password: string) {
 	return db
 		.prepare(`INSERT INTO users (image, name, email, password) VALUES(?, ?, ?, ?)`)
@@ -20,4 +22,10 @@ export function getUserById(id: number) {
 		.get(id) as { name: string; id: number; image: string };
 }
 
-export function checkPassword() {}
+export async function hashPassword(password: string) {
+	return await bcrypt.hash(password, 10);
+}
+
+export async function verifyPassword(password: string, hash: string) {
+	return await bcrypt.compare(password, hash);
+}
