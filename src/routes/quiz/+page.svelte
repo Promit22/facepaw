10ad846d2b5411111d8df40383452c30a7cbbd3c
Promit Barua Antu <promitbarua796@gmail.com>
2026-03-03 22:37:12 +0600

    ongoing changes in quiz

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
 	import { goto } from '$app/navigation';
+	import hero from '$lib/assets/quizHero.webp';
 	let { data }: PageProps = $props();
 </script>
 
 <article class=" mt-5 h-full w-full rounded-2xl bg-amber-100">
-	<section class=" flex flex-col items-center justify-center gap-1.5">
-		<h1 class=" self-start text-3xl">FacePaw</h1>
-		<h2 class=" text-4xl">Ready to test your knowlwdge about cat and dog breed?</h2>
-		<p class=" font-light">Choose a mode and see how well you really know your paws.</p>
+	<section class=" flex flex-row items-center justify-center gap-1.5">
+		<div class=" flex-1 text-center">
+			<h1 class="text-5xl/16">Ready to test your breed knowledge?</h1>
+			<p class=" text-foreground">Select one breed and prove</p>
+		</div>
+		<div class=" relative flex-2">
+			<img src={hero} alt="" />
+			<span class=" absolute right-2 bottom-0.5">Image by Gemini</span>
+		</div>
 	</section>
 </article>
